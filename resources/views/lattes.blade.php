d8145d12eb97c15956263613149654977e12c53d
Murilo Ialamov <murilo.ialamov@gmail.com>
2025-09-15 15:55:54 -0300

    remover lattes view

--- resources/views/lattes.blade.php
+++
@@ -1,7 +0,0 @@
-<h1>Currículo Lattes</h1>
-
-<ul>
-    <li>Nome: {{ $dados['nome'] ?? 'Não disponível' }}</li>
-    <li>Data de atualização: {{ \Carbon\Carbon::parse($dados['data_atualizacao'])->format('d/m/Y') }}</li>
-    <li>Link: <a href="{{ $dados['url'] }}">{{ $dados['url'] }}</a></li>
-</ul>
